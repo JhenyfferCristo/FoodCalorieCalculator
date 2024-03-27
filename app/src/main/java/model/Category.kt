f370c781a6e8738ce2789cce1157d13073b1876f
Vergil Phan <123117700+vergilht@users.noreply.github.com>
2024-03-27 05:53:51 -0700

    Delete app/src/main/java/model directory

--- app/src/main/java/model/Category.kt
+++
@@ -1,9 +0,0 @@
-package model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-class Category (
-    @StringRes val nameRes: Int,
-    @DrawableRes val imageRes: Int
-)
